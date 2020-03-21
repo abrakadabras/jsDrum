327d97649c24be80fb7298e9d77259a47febf356
Joshua <tsym88@gmail.com>
2020-03-21 16:59:01 -0400

    first commit jsDrum

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function playSound(e){
+    let audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.toggle('playing');
+};
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound);
